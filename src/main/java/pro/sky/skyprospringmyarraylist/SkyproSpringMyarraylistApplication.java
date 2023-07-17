2c3725ff541da5bcd94a3c686f6d1cb4e41950b9
margo <margo171190@mail.ru>
2023-07-17 08:24:08 +0400

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringmyarraylist/SkyproSpringMyarraylistApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringmyarraylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringMyarraylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringMyarraylistApplication.class, args);
+	}
+
+}
